fa3a6ad479bd0058d118e4a9615368d553026fd6
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2019-01-14 17:21:10 -0200

    :art: refactor #1

--- src/components/CarouselItem.vue
+++ src/components/CarouselItem.vue
@@ -1,10 +1,5 @@
 <template>
-  <div
-    class="carousel-item"
-    :style="{ flex }"
-    @mousedown="mousedown"
-    @mouseup="mouseup"
-  >
+  <div class="carousel-item" :style="{ flex }">
     <slot />
   </div>
 </template>
@@ -20,27 +15,6 @@ export default {
 
     isDraggable () {
       return this.$parent.isDraggable && !this.$parent.isLoopable
-    }
-  },
-
-  methods: {
-    mousemove (e) {
-      this.$emit('move-position', e)
-    },
-
-    mousedown (e) {
-      if (!this.isDraggable) return false
-
-      this.$emit('init-position', e)
-
-      window.addEventListener('mousemove', this.mousemove)
-      window.addEventListener('mouseup', () => window.removeEventListener('mousemove', this.mousemove))
-    },
-
-    mouseup (e) { // touchend ?
-      if (!this.isDraggable) return false
-
-      this.$emit('set-position', e)
     }
   }
 }
