fa4c65e15edf1a5188604171c140d7f25c12c023
Vinicius Azevedo <viniazvd@gmail.com>
2019-12-29 19:48:01 -0300

    :sparkles: create setProvide

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,13 @@
 export const isMobile = () => {
   return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp2|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
 }
+
+export const setProvide = configs => {
+  const options = {}
+
+  const define = (name, get) => Object.defineProperty(options, name, { get, enumerable: true })
+
+  configs.forEach(({ name, get }) => define(name, () => get))
+
+  return options
+}
