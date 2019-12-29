46ea7fe94fc76e52f22b761c13e89c25869ebb63
Vinicius Azevedo <viniazvd@gmail.com>
2019-12-29 19:52:11 -0300

    :sparkles: create setEvents

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,9 @@
-export const isMobile = () => {
-  return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp2|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+const isMobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp2|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+
+export const setEvents = {
+  'start': isMobile ? 'touchstart' : 'mousedown',
+  'move': isMobile ? 'touchmove' : 'mousemove',
+  'end': isMobile ? 'touchend' : 'mouseup'
 }
 
 export const setProvide = configs => {
