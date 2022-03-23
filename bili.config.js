fb8e10b200a51a47224a638bc0e461ede41e3746
viniazvd <viniazvd@gmail.com>
2022-03-23 09:29:40 -0300

    :wrench: remove style config

--- bili.config.js
+++ bili.config.js
@@ -1,21 +1,12 @@
 'use strict'
 
 const Vue = require('rollup-plugin-vue')
-const autoprefixer = require('autoprefixer')
 
 module.exports = {
   js: 'buble',
   input: 'src/index.js',
   format: [ 'es', 'cjs', 'umd' ],
   plugins: [
-    Vue({
-      css: false,
-      style: {
-        trim: false,
-        postcssPlugins: [
-          autoprefixer({ browsers: [ '> 0.000001%' ] })
-        ]
-      }
-    })
+    Vue()
   ]
 }
